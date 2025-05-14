6e36b53bcf6c18b801299fd8258b395862e81416
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2025-05-14 14:26:32 +0300

    API functions for database handling added

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,9 +1,35 @@
 import Database from "better-sqlite3"
 
-const db = new Database('./sqlite.db')
+const db = new Database("./sqlite.db")
 
-export async function GET() {
-    const users = db.prepare('SELECT * FROM user').all()
-    console.log(users)
+// Hämtar från tabellen users alla rader
+export async function GET(){
+    const users = db.prepare("SELECT * FROM todos").all()
+    //skickar tillbaka json respons
     return Response.json(users)
+}
+
+// Skapar en ny todo
+export async function POST(req: Request){
+    // Läser in request body
+    const body = await req.json()
+    const data = db.prepare("INSERT INTO todos (title, todo) VALUES (?, ?)").run(body.title, body.todo)
+    //skcikar tillbaka json respons
+    return Response.json(data)
+}
+
+// Tar bort en todo
+export async function DELETE(req: Request){
+    // Läser in request body
+    const body = await req.json()
+    const data = db.prepare("DELETE FROM todos WHERE id = ?").run(body)
+    return Response.json(data)
+}
+// Updatarar en todo
+export async function PUT(req: Request){
+    // Läser in request body
+    const body = await req.json()
+    console.log('body', body)
+    const data = db.prepare("UPDATE todos SET title = ?, todo = ? WHERE id = ?").run(body.title, body.todo, body.id)
+    return Response.json(data)
 }
